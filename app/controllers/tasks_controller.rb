54dc1ee9144c35383bd5df485150ac43f824b722
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-01-07 15:03:57 +0900

    maketaskactionflame

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,48 @@
 class TasksController < ApplicationController
+  
   def index
+    @task=Task.all
   end
 
   def new
+    @task=Task.new
+  end
+
+  def create
+    @task=Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path, notice: "投稿しました"
+    else
+      render 'new'
+    end
   end
 
   def show
+    @task=Task.find(params[:id])
   end
 
   def edit
+    @task=Task.find(params[:id])
+  end
+
+  def update
+    @task=Task.find(params[:id])
+
+    if @task.update
+      redirect_to tasks_path, notice: "投稿しました"
+    else
+      render 'edit'
+    end
   end
 
   def confirm
+    @task=Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :image)
   end
 end
