4a08b3d0399de09dec6b30a8546758ee435a1f65
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-01-08 14:22:58 +0900

    edituserindexview

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_053652) do
+ActiveRecord::Schema.define(version: 2020_01_08_035913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_01_07_053652) do
   create_table "tasks", force: :cascade do |t|
     t.text "content", null: false
     t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
