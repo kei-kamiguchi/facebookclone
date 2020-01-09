cadedaab34ae540361b7bf3d7698f339ba6ca65b
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-01-09 11:32:53 +0900

    addacountlink

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :tasks do
@@ -6,5 +7,4 @@ Rails.application.routes.draw do
       post 'confirm'
     end
   end
-  root to: 'sessions#new'
 end
